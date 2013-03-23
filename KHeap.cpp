08013ccb9fb4092be0fb37eecbf4a5ed4497bde5
Philippe Daouadi <p.daouadi@free.fr>
2013-03-23 12:09:13 +0100

    Reimplemented kmalloc, still incomplete and untested

--- KHeap.cpp
+++ KHeap.cpp
@@ -1,31 +1,64 @@
-// kheap.c -- Kernel heap functions, also provides
-//            a placement malloc() for use before the heap is 
-//            initialised.
-//            Written for JamesM's kernel development tutorials.
-
 #include "KHeap.hpp"
+
+// XXX duplicated from Multiboot
+template <typename T>
+inline T alignSup(T base, uint8_t val)
+{
+  return (base + val-1) & ~(uint64_t)(val-1);
+}
 
 extern "C" void* kernelEndAddress;
 
 // XXX
-char* KHeap::placement_address = (char*)0x120000;//&kernelEndAddress;
+//char* KHeap::placement_address = (char*)0x120000;//&kernelEndAddress;
 
-void* KHeap::kmalloc(uint32_t sz, void** phys)
+// TODO initialize these
+uint8_t* KHeap::m_heapStart;
+uint8_t* KHeap::m_heapEnd;
+
+void* KHeap::kmalloc(uint32_t size)
 {
-  if (phys)
-    *phys = placement_address;
-  char* tmp = placement_address;
-  placement_address += sz;
-  return tmp;
+  size += 4;
+
+  uint8_t* ptr = m_heapStart;
+  while (ptr < m_heapEnd)
+  {
+    HeapBlock* block = reinterpret_cast<HeapBlock*>(ptr);
+    uint32_t blockSize = block->size & ~0x3;
+
+    if (!(block->size & HEAP_USED))
+    {
+      if (size == blockSize)
+      {
+        block->size |= HEAP_USED;
+        return &block->data;
+      }
+      else if (size <= blockSize - 8)
+      {
+        uint32_t firstBlockSize = alignSup(size, 4);
+        uint32_t fullSize = blockSize;
+        HeapBlock* nextBlock = reinterpret_cast<HeapBlock*>(
+            ptr + firstBlockSize);
+
+        block->size = firstBlockSize | HEAP_USED;
+        nextBlock->size = fullSize - size;
+
+        //TODO uncomment when assert is implemented
+        //assert(!(nextBlock->size & 0x3));
+
+        return &block->data;
+      }
+    }
+
+    ptr += block->size;
+  }
+
+  // TODO enlarge heap to allocate block
+  return nullptr;
 }
 
 void* KHeap::kmalloc_a(uint32_t sz, void** phys)
 {
-  // This will eventually call malloc() on the kernel heap.
-  // For now, though, we just assign memory at placement_address
-  // and increment it by sz. Even when we've coded our kernel
-  // heap, this will be useful for use before the heap is initialised.
-  placement_address = (char*)(
-      ((long)(placement_address + (0x1000 - 1))) & ~0xFFFL);
-  return kmalloc(sz, phys);
+  // TODO
+  return nullptr;
 }
--- KHeap.hpp
+++ KHeap.hpp
@@ -6,11 +6,20 @@
 class KHeap
 {
   public:
-    static void* kmalloc(uint32_t sz, void** phys = nullptr);
+    static void* kmalloc(uint32_t size);
     static void* kmalloc_a(uint32_t sz, void** phys = nullptr);
 
   private:
-    static char* placement_address;
+    static constexpr uint32_t HEAP_USED = 0x1;
+
+    struct HeapBlock
+    {
+      uint32_t size;
+      uint8_t data;
+    } __attribute__((packed));
+
+    static uint8_t* m_heapStart;
+    static uint8_t* m_heapEnd;
 };
 
 #endif /* K_HEAP_HPP */
