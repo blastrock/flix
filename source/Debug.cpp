02b6e4731eee5c87b020ab2c1f38bb7372a5c719
Philippe Daouadi <p.daouadi@free.fr>
2015-04-14 21:32:38 +0200

    Prevent stack unwinding from looping infinitely

--- source/Debug.cpp
+++ source/Debug.cpp
@@ -8,6 +8,11 @@ void printStackTrace(uint64_t stackPointer)
   while (stackFrame[1])
   {
     Degf("%p", stackFrame[1]);
+    if (stackFrame >= stackFrame[0])
+    {
+      Degf("stack fail");
+      return;
+    }
     stackFrame = static_cast<void**>(stackFrame[0]);
   }
 }
