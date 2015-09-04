8a2ab693514edd39295726dc781e532bdf2543fc
Philippe Daouadi <p.daouadi@free.fr>
2015-09-04 22:30:02 +0200

    Use pnt from xll namespace

--- source/Dmesg.hpp
+++ source/Dmesg.hpp
@@ -4,13 +4,13 @@
 #include <sstream>
 #include <cstring>
 #include "ConsoleStreamBuf.hpp"
-#include "pnt.hpp"
+#include <xll/pnt.hpp>
 
 template <typename... Args>
 void Degf(const char* fmt, const Args&... args)
 {
   ConsoleStreamBuf st;
-  pnt::writef(st, fmt, args...);
+  xll::pnt::writef(st, fmt, args...);
   st.sputc('\n');
 }
 
