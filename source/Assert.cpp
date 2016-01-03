e747cac28baa51bae40a38114169200a53c5a660
Philippe Daouadi <p.daouadi@free.fr>
2016-01-03 22:08:14 +0100

    Assert: disable interrupts when printing assertion failure message

--- source/Assert.cpp
+++ source/Assert.cpp
@@ -1,10 +1,12 @@
 #include <cassert>
+#include "Util.hpp"
 #include "Debug.hpp"
 
 XLL_LOG_CATEGORY("support/assert");
 
 extern "C" void assert_fail(const char* file, int line, const char* condition)
 {
+  disableInterrupts();
   xDeb("%s:%d\nAssertion failed: %s", file, line, condition);
   printStackTrace();
   PANIC("Assertion failed");
