a348d7860c5495d206f79dc0df2a4b4e2d22290b
Philippe Daouadi <p.daouadi@free.fr>
2016-01-03 19:24:18 +0100

    test_multitask: simplified

--- source/test/multitask.cpp
+++ source/test/multitask.cpp
@@ -2,36 +2,26 @@
 #include "TaskManager.hpp"
 #include "Screen.hpp"
 #include "Syscall.hpp"
-#include "Symbols.hpp"
-#include "Elf.hpp"
+#include "helpers.hpp"
 
 XLL_LOG_CATEGORY("main");
 
-volatile bool finish1 = false;
-volatile bool finish2 = false;
-
-void waitEnd()
-{
-  while (!finish1 || !finish2)
-    ;
-  printE9("\n[END TEST]\n");
-  printE9("\n[ALL TESTS RUN]\n");
-  sys::call(sys::exit);
-}
-
-void loop()
+void loop1()
 {
   for (unsigned int i = 0; i < 800; ++i)
     xDeb("stuff %d", i++);
-  finish1 = true;
-  sys::call(sys::exit);
 }
 
 void loop2()
 {
   for (unsigned int i = 0; i < 800; ++i)
     xDeb("different stuff %d", i++);
-  finish2 = true;
+}
+
+void waitEnd()
+{
+  th::runTestProcesses("double_loop", loop1, loop2);
+  th::finish();
   sys::call(sys::exit);
 }
 
@@ -40,52 +30,22 @@ void loop2()
   Screen::clear();
   Screen::putString("Booting Flix\n");
 
-  auto taskManager = TaskManager::get();
-
-  printE9("\n[BEGIN TEST \"multitask\"]\n");
+  auto& taskManager = *TaskManager::get();
 
   {
-    Task task = taskManager->newKernelTask();
-    task.stack = reinterpret_cast<char*>(0xffffffffa0000000 - 0x4000);
+    Task task = taskManager.newKernelTask();
+    task.stack = static_cast<char*>(getStackPageHeap().kmalloc().first);
     task.stackTop = task.stack + 0x4000;
     task.kernelStack = static_cast<char*>(getStackPageHeap().kmalloc().first);
     task.kernelStackTop = task.kernelStack + 0x4000;
-    task.pageDirectory.mapRange(task.stack, task.stackTop,
-        PageDirectory::ATTR_RW);
     task.context.rsp = reinterpret_cast<uint64_t>(task.stackTop);
     task.context.rip = reinterpret_cast<uint64_t>(&waitEnd);
-    taskManager->addTask(std::move(task));
-  }
-  {
-    Task task = taskManager->newKernelTask();
-    task.stack = reinterpret_cast<char*>(0xffffffffa0000000 - 0x4000);
-    task.stackTop = task.stack + 0x4000;
-    task.kernelStack = static_cast<char*>(getStackPageHeap().kmalloc().first);
-    task.kernelStackTop = task.kernelStack + 0x4000;
-    task.pageDirectory.mapRange(task.stack, task.stackTop,
-        PageDirectory::ATTR_RW);
-    task.context.rsp = reinterpret_cast<uint64_t>(task.stackTop);
-    task.context.rip = reinterpret_cast<uint64_t>(&loop);
-    task.hh.parent = taskManager->getTask(1);
-    taskManager->addTask(std::move(task));
-  }
-  {
-    Task task = taskManager->newKernelTask();
-    task.stack = reinterpret_cast<char*>(0xffffffffa0000000 - 0x4000);
-    task.stackTop = task.stack + 0x4000;
-    task.kernelStack = static_cast<char*>(getStackPageHeap().kmalloc().first);
-    task.kernelStackTop = task.kernelStack + 0x4000;
-    task.pageDirectory.mapRange(task.stack, task.stackTop,
-        PageDirectory::ATTR_RW);
-    task.context.rsp = reinterpret_cast<uint64_t>(task.stackTop);
-    task.context.rip = reinterpret_cast<uint64_t>(&loop2);
-    task.hh.parent = taskManager->getTask(1);
-    taskManager->addTask(std::move(task));
+    taskManager.addTask(std::move(task));
   }
 
   xInf("End of kernel");
 
-  taskManager->scheduleNext(); // start a task, never returns
+  taskManager.scheduleNext(); // start a task, never returns
 
   // this stack will be reused for interrupts when there is no active task
 
